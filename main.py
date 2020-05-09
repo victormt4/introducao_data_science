bcc697491f7383f357441a3501e952835c226174
Victor Mota <victor.vmt@outlook.com>
2020-05-09 10:40:14 -0300

    Adicionando comentários

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-kms = np.loadtxt('data/carros-km.txt')
-anos = np.loadtxt('data/carros-anos.txt', dtype=int)
+kms = np.loadtxt(fname='data/carros-km.txt', dtype=int)
+anos = np.loadtxt(fname='data/carros-anos.txt', dtype=int)
 
 km_media = kms / (2019 - anos)  # executa km / (2020 - ano) para cada item do vetor
 
 print(km_media)
+print(km_media.shape)  # dimensões do array
