ae6d5853eb206a7926085d92dd7bc1409a16494f
Victor Mota <victor.vmt@outlook.com>
2020-05-12 18:42:22 -0300

    Adicionando exemplos de uso do Pandas

--- exemplos_pd.py
+++ exemplos_pd.py
@@ -13,3 +13,28 @@ print(dataset[['Quilometragem', 'Valor']].describe())
 
 print('\nInfo gerais:\n')
 print(dataset.info())
+
+print('\nCriando DataFrame\n')
+# Usando uma lista de dicionário
+dados = [
+    {'Nome': 'Jetta Variant', 'Motor': 'Motor 4.0 Turbo', 'Ano': 2003, 'Quilometragem': 44410.0, 'Zero_km': False,
+     'Valor': 88078.64},
+    {'Nome': 'Passat', 'Motor': 'Motor Diesel', 'Ano': 1991, 'Quilometragem': 5712.0, 'Zero_km': False,
+     'Valor': 106161.94},
+    {'Nome': 'Crossfox', 'Motor': 'Motor Diesel V8', 'Ano': 1990, 'Quilometragem': 37123.0, 'Zero_km': False,
+     'Valor': 72832.16}
+]
+# Usando um dicionário de listas
+dados = {
+    'Nome': ['Jetta Variant', 'Passat', 'Crossfox'],
+    'Motor': ['Motor 4.0 Turbo', 'Motor Diesel', 'Motor Diesel V8'],
+    'Ano': [2003, 1991, 1990],
+    'Quilometragem': [44410.0, 5712.0, 37123.0],
+    'Zero_km': [False, False, False],
+    'Valor': [88078.64, 106161.94, 72832.16]
+}
+dataset = pd.DataFrame(dados)
+print(dataset)
+
+print('\nAlterando ordem das colunas:\n')
+print(dataset[['Nome', 'Ano', 'Quilometragem', 'Valor', 'Zero_km', 'Motor']])
